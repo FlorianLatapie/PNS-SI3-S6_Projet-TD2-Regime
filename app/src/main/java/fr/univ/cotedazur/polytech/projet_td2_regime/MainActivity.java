b4a8b273213ea8e0f78bee3daba2ec1c0b6eb54d
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-03-02 17:19:01 +0100

    Initial commit

---
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.univ.cotedazur.polytech.projet_td2_regime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
