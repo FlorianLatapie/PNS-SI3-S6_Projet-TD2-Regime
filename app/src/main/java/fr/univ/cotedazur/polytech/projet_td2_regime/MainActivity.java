ab24c162370ce365328d59a31eaf2f305b506e7e
tom3883 <thomaspaul987@gmail.com>
2022-03-17 17:56:15 +0100

    ADD navigation bar V1

--- app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/MainActivity.java
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/MainActivity.java
@@ -1,8 +1,16 @@
 package fr.univ.cotedazur.polytech.projet_td2_regime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.*;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
--- app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/home/IListner.java
+++
@@ -1,5 +0,0 @@
-package fr.univ.cotedazur.polytech.projet_td2_regime.home;
-
-public interface IListner {
-    public void onClickMeal(int position);
-}
