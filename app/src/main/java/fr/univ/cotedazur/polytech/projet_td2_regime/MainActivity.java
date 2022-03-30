6c1e9fd8867e41760bdf59f9e1f91d4f596f3ab6
Florian Latapie <florian.latapie@etu.unice.fr>
2022-03-30 16:20:46 +0200

    top bar mais elle clique pas

--- app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/MainActivity.java
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/MainActivity.java
@@ -1,29 +1,37 @@
 package fr.univ.cotedazur.polytech.projet_td2_regime;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.*;
 import androidx.navigation.NavController;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
     }
 }
---
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/create_meal/CreateMealActivity.java
@@ -0,0 +1,7 @@
+package fr.univ.cotedazur.polytech.projet_td2_regime.create_meal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateMealActivity extends AppCompatActivity {
+
+}
