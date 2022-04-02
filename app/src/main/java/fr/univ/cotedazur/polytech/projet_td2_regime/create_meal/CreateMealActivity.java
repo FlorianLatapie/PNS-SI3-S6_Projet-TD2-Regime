420834aa5dfccfec13116cf34c9704029d978402
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-02 19:47:01 +0200

    quand on prend une photo ca l'affiche sur l'activité

--- app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/create_meal/CreateMealActivity.java
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/create_meal/CreateMealActivity.java
@@ -1,15 +1,85 @@
 package fr.univ.cotedazur.polytech.projet_td2_regime.create_meal;
 
+import static fr.univ.cotedazur.polytech.projet_td2_regime.create_meal.IPictureActivity.CAMERA_REQUEST_CODE;
+import static fr.univ.cotedazur.polytech.projet_td2_regime.create_meal.IPictureActivity.IMAGE_PICK_GALLERY_CODE;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import fr.univ.cotedazur.polytech.projet_td2_regime.R;
 
 public class CreateMealActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button prendrePhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meal);
+        imageView = findViewById(R.id.imagePreview);
+
+        prendrePhoto = findViewById(R.id.addImageButton);
+        prendrePhoto.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+            } else {
+                openCamera();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case IMAGE_PICK_GALLERY_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+                } else {
+                    Toast.makeText(this, "Gallery permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                imageView.setImageBitmap((Bitmap) data.getExtras().get("data"));
+            } else if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                imageView.setImageURI(data.getData());
+            }
+        }
     }
 }
