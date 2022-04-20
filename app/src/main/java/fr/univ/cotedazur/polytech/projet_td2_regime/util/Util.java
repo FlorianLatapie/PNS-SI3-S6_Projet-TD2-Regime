3b2e0530f9b785800cdec0d3a7f8c725767a8ce7
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-20 16:00:50 +0200

    Create Util.java

---
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/util/Util.java
@@ -0,0 +1,9 @@
+package fr.univ.cotedazur.polytech.projet_td2_regime.util;
+
+public class Util {
+
+    // replaces all the occurences of a " " with a "%20"
+    public static String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
