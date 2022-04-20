5d26284746437871d8163b38db9344511221c44d
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-20 15:24:34 +0200

    Update Diet.java

--- app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/profile/Diet.java
+++ app/src/main/java/fr/univ/cotedazur/polytech/projet_td2_regime/profile/Diet.java
@@ -1,24 +1,40 @@
 package fr.univ.cotedazur.polytech.projet_td2_regime.profile;
 
 public enum Diet {
-    VEGETARIAN("Vegetarien"),
-    GLUTEN_FREE("Sans gluten"),
-    PROTEIN("Sans proteine");
+    VEGETARIAN("Vegetarien", "Vegetarian"),
+    GLUTEN_FREE("Sans gluten", "Gluten-free"),
+    PROTEIN("Sans proteine", "Protein-free"),
+    NONE("Aucun", "None");
+
     private final String name;
+    private final String englishName;
     public static final Diet[] values = values();
 
-    Diet(String name) {
+    Diet(String name, String englishName) {
         this.name = name;
+        this.englishName = englishName;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getEnglishName() {
+        return englishName;
+    }
+
     public static String[] getNames() {
         String[] names = new String[values.length];
         for (int i = 0; i < values.length; i++) {
             names[i] = values[i].getName();
+        }
+        return names;
+    }
+
+    public static String[] getEnglishNames() {
+        String[] names = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            names[i] = values[i].getEnglishName();
         }
         return names;
     }
